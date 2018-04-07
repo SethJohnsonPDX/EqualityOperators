4bd393bd6bb909ce94483844e3b6e4da5bc06bee
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-07 16:50:43 -0700

    commit all

--- EqualityOperators/Program.cs
+++ EqualityOperators/Program.cs
@@ -10,6 +10,24 @@ namespace EqualityOperators
     {
         static void Main(string[] args)
         {
+            bool greaterAnd = (2 > 2) && (5 > 4);
+
+            Console.WriteLine(greaterAnd);
+            Console.ReadLine();
+            // Should give False
+
+            bool greaterOr = (3 > 2) || (4 > 4);
+
+            Console.WriteLine(greaterOr);
+            Console.ReadLine();
+            // Should give True
+
+            bool notEqual = (2 + 2) != 4;
+
+            Console.WriteLine(notEqual);
+            Console.ReadLine();
+            // Should give False
+
         }
     }
 }
